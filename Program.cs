ec4ffbedfa56560030eef635a0c61e231424ec50
kuznecovae903 <kuznecovae903@gmail.com>
2022-09-18 23:39:24 +0300

    внесены изменения

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string username = Console.ReadLine(ВВедите Ваше имя);
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
